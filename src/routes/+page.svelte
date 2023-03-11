82923fa86c9a6df10bd52c9d251c0706e437e314
Fredrik Blomqvist <frblo@kth.se>
2023-03-11 22:37:27 +0100

    Clean up and comment

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -2,19 +2,26 @@
 <!-- <p>Smaka bly, bitch!</p> -->
 
 <script lang="ts">
-	let title = "";
-	let typeSpeed = 100;
-	title = "";
-
+	let title = ""; // The title on the page
+	let typeSpeed = 100; // The speed of the typewriter effect
 
 	function handleClick() {
 		writeTitle("dionysus");
 	}
 
+	/**
+	 * Change the title on the page
+	 * @param newTitle the new title
+	 */
 	function writeTitle(newTitle:string) {
 		let i = 0;
 		writeChar();
 
+		/**
+		 * The typewriting effect itself. Writes out
+		 * a character at a time and calls itself with
+		 * a timer
+		 */
 		function writeChar() {
 			title += newTitle[i]
 			i++;
@@ -24,6 +31,7 @@
 		}
 	}
 
+	// Writes the title on load
 	writeTitle("dionysus");
 </script>
 
@@ -35,13 +43,8 @@
 	>
 		{title}
 	</h1>
+
 	<button style="display: block; margin: auto;" on:click={handleClick}>
 		write title
 	</button>
 </div>
-
-<!-- <style>
-	body {
-		background-color:#707070;
-	}
-</style> -->
