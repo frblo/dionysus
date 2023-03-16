9d6163ceab13c408d47f1244a8302c4f13598834
Fredrik Blomqvist <frblo@kth.se>
2023-03-16 12:44:58 +0100

    Better error page

--- src/routes/+error.svelte
+++ src/routes/+error.svelte
@@ -1,1 +1,16 @@
-<h1>Wow, vilket fet fel!</h1>
+<script>
+    import {page} from '$app/stores';
+</script>
+
+{#if $page.error}
+    <h1>{$page.status}: {$page.error.message}</h1>
+{:else}
+    <h1>An error has occurred</h1>
+{/if}
+
+<style>
+    h1 {
+        margin-top: 250px;
+       text-align: center;
+    }
+</style>
