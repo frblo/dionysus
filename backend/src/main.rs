9f936db200c8a71cc5930c50bbb2842ab75c08ea
Fredrik Blomqvist <scanmar333@gmail.com>
2026-02-10 21:40:27 +0100

    Serve frontend through backend
    Cuts down from two Dockerfiles, one for the frontend and backend each,
    to only a single one which handles both.

--- backend/src/main.rs
+++ backend/src/main.rs
@@ -5,11 +5,13 @@ mod rooms;
 mod state;
 mod ws;
 
-use std::net::SocketAddr;
+use std::{env, net::SocketAddr};
 
 use sqlx::PgPool;
 
 use crate::db::Db;
+
+const SERVE_FRONTEND_ARG: &str = "serve";
 
 #[tokio::main]
 async fn main() -> anyhow::Result<()> {
@@ -18,7 +20,20 @@ async fn main() -> anyhow::Result<()> {
 
     let state = state::AppState::new(Db::new(pool)).await;
 
-    let app = app::router(state);
+    let args: Vec<String> = env::args().collect();
+
+    let serve_frontend = match &args.get(1) {
+        Some(s) => {
+            if s.as_str() == SERVE_FRONTEND_ARG {
+                true
+            } else {
+                false
+            }
+        }
+        _ => false,
+    };
+
+    let app = app::router(state, serve_frontend);
 
     let listener = tokio::net::TcpListener::bind(addr).await.unwrap();
     println!("Listening on {}", listener.local_addr().unwrap());
