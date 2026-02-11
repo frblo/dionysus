17acd215c8814a039c6dd0ea4fe92edd8788b7f6
Fredrik Blomqvist <scanmar333@gmail.com>
2026-02-11 16:23:26 +0100

    Add a simple page title
    Fixes #37. This adds a very basic page title; "Dionysus". When we allow
    for multiple files this should be improved to include those file names.
    It's different on the login page, where it features a "Login - " prefix.

--- frontend/src/routes/+layout.svelte
+++ frontend/src/routes/+layout.svelte
@@ -7,6 +7,7 @@
 
 <svelte:head>
   <link rel="icon" href={favicon} />
+  <title>Dionysus</title>
 </svelte:head>
 
 <div
---
+++ frontend/src/routes/login/+layout.svelte
@@ -0,0 +1,3 @@
+<svelte:head>
+  <title>Login - Dionysus</title>
+</svelte:head>
