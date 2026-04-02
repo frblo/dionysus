79178e1d4e8aa73d9ff5cbc72ed736979b8aa6f1
Fredrik Blomqvist <scanmar333@gmail.com>
2026-04-02 15:34:28 +0200

    Update Rustwell to 0.3.0
    Updates Rustwell, bringing with it significantly faster build times and
    export times for `pdf`s. Also fixes #68.

--- frontend/src/lib/converter/src/lib.rs
+++ frontend/src/lib/converter/src/lib.rs
@@ -1,15 +1,18 @@
-use rustwell::{export_html, export_pdf, parse};
+use rustwell::{A4, Exporter, ExporterExt, HtmlExporter, PdfExporter, parse};
 use wasm_bindgen::prelude::*;
 
 /// Runs the Rustwell fountain parser and compiles the script into stylized HTML.
 #[wasm_bindgen]
 pub fn generate_html(fountain: String) -> Result<String, JsValue> {
     let parsed = parse(fountain);
-    let mut buffer = Vec::new();
-    export_html(&parsed, &mut buffer, true, false);
-
-    let html_string =
-        String::from_utf8(buffer).map_err(|_| JsValue::from_str("Invalid UTF-8 sequence"))?;
+    let exporter = HtmlExporter {
+        standalone: true,
+        synopses: false,
+        include_source_positions: false,
+    };
+    let html_string = exporter
+        .export_to_string(&parsed)
+        .map_err(|_| JsValue::from_str("Invalid UTF-8 sequence"))?;
 
     Ok(html_string)
 }
@@ -19,7 +22,13 @@ pub fn generate_html(fountain: String) -> Result<String, JsValue> {
 pub fn generate_pdf(fountain: String) -> Result<Vec<u8>, JsValue> {
     let parsed = parse(fountain);
     let mut buffer = Vec::new();
-    export_pdf(&parsed, &mut buffer, false);
+    let exporter = PdfExporter {
+        synopses: false,
+        paper_size: A4,
+    };
+    exporter
+        .export(&parsed, &mut buffer)
+        .map_err(|_| JsValue::from_str("Invalid UTF-8 sequence"))?;
 
     Ok(buffer)
 }
