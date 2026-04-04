7b68804a2cff843d808480fffab181765530e456
Emil Hultcrantz <emilhtz@gmail.com>
2026-04-04 12:56:54 +0200

    Change preview to not display in an iframe

--- frontend/src/lib/converter/src/lib.rs
+++ frontend/src/lib/converter/src/lib.rs
@@ -3,7 +3,7 @@ use wasm_bindgen::prelude::*;
 
 /// Runs the Rustwell fountain parser and compiles the script into stylized HTML.
 #[wasm_bindgen]
-pub fn generate_html(fountain: String) -> Result<String, JsValue> {
+pub fn generate_standalone_html(fountain: String) -> Result<String, JsValue> {
     let parsed = parse(fountain);
     let exporter = HtmlExporter {
         standalone: true,
@@ -12,6 +12,28 @@ pub fn generate_html(fountain: String) -> Result<String, JsValue> {
     };
     let html_string = exporter
         .export_to_string(&parsed)
+        .map_err(|_| JsValue::from_str("Invalid UTF-8 sequence"))?;
+
+    Ok(html_string)
+}
+
+/// Gets the standard `css` for Rustwell `html`.
+#[wasm_bindgen]
+pub fn get_css() -> String {
+    HtmlExporter::export_css().to_string()
+}
+
+/// Generates the `html` representation of the script using Rustwell.
+#[wasm_bindgen]
+pub fn generate_html(fountain: String) -> Result<String, JsValue> {
+    let screenplay = parse(fountain);
+    let exporter = HtmlExporter {
+        standalone: false,
+        synopses: false,
+        include_source_positions: false,
+    };
+    let html_string = exporter
+        .export_to_string(&screenplay)
         .map_err(|_| JsValue::from_str("Invalid UTF-8 sequence"))?;
 
     Ok(html_string)
